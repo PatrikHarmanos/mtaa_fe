e6f179e86393321396106fccb402c55dc5156e4b
Patrik Harmanos <harmanos.patrik@gmail.com>
2022-04-04 13:27:59 +0200

    added products (menu) page

--- Screens/MenuScreen.js
+++ Screens/MenuScreen.js
@@ -1,24 +1,67 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {
   StyleSheet,
-  TextInput,
   View,
   Text,
-  Image,
-  KeyboardAvoidingView,
-  Keyboard,
-  TouchableOpacity,
-  ScrollView,
-  Button,
-  Dimensions,
-  Platform
+  SectionList,
+  TouchableOpacity
 } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 const MenuScreen = ({navigation}) => {
+
+  const [DATA, setDATA] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const onRefresh = () => {
+    setIsFetching(true);
+  }
+
+  useEffect(() => {
+    try {
+      SecureStore.getItemAsync('access').then((token) => {
+        if (token != null) {
+          fetch('http://localhost:3000/api/products/get_products', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token,
+            }
+          })
+            .then((response) => response.json())
+            .then ((responseJson) => {
+              setDATA(responseJson)
+            })
+        } else {
+          navigation.navigate("SplashScreen")
+        }
+      })
+    } catch(error) {
+      console.log(error);
+    }
+  }, [isFetching])
+
+  const Item = ({ item }) => (
+    <TouchableOpacity onPress={() => console.log('touched')} style={styles.item}>
+      <View>
+        <Text style={styles.orderTitle}>{item.name}</Text>
+        <Text style={styles.orderSubTitle}>{item.description}</Text>
+      </View>
+      <Text style={styles.orderSubTitle}>{item.price} €</Text>
+    </TouchableOpacity>
+  );
   
   return (
     <View style={styles.container}>
       <Text style={styles.textHeading}>Aktuálna ponuka</Text>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item.id + index}
+        renderItem={({ item }) => <Item item={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.categoryText}>{title}</Text>
+        )}
+      />
     </View>
   );
 };
@@ -29,21 +72,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  header: {
-    paddingTop: 70,
-    flex: 4,
-    justifyContent: 'flex-end',
-    paddingHorizontal: 20,
-    paddingBottom: 40
-  },
-  footer: {
-    flex: 1,
-    backgroundColor: '#5B84B1FF',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 30,
-  },
   textHeading: {
     color: '#5B84B1FF',
     fontSize: 20,
@@ -51,45 +79,33 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginTop: 10
   },
-  text_footer: {
-    color: '#05375a',
-    fontSize: 18,
-    marginLeft: 20
-  },
-  action: {
-    flexDirection: 'row',
-    marginTop: 5,
-    marginBottom: 20,
-    marginHorizontal: 20,
-    paddingBottom: 5,
+  item: {
     backgroundColor: '#fff',
-    borderRadius: 5,
-    borderColor: '#777',
-    borderWidth: 1
-  },
-  textInput: {
-    flex: 1,
-    marginVertical: 12,
-    paddingLeft: 10,
-    color: '#05375a',
-    fontSize: 16
-  },
-  button: {
-    alignItems: 'center',
-    marginTop: 40
-  },
-  signIn: {
-    width: 300,
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
+    padding: 15,  
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+    display: 'flex',
     flexDirection: 'row',
-    backgroundColor: '#fff'
+    justifyContent: 'space-between'
   },
-  textSign: {
+  orderTitle: {
+    fontSize: 21,
+    marginBottom: 4,
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  orderSubTitle: {
+    fontSize: 16,
     color: '#5B84B1FF',
+    fontWeight: 'bold'
+  },
+  categoryText: {
+    marginLeft: 20,
+    marginTop: 20,
     fontWeight: 'bold',
-    fontSize: 20
+    color: '#006902',
+    fontSize: 25
   }
 });
